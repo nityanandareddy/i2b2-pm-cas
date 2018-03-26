38ded41c91304637e33d6722220e790ad3b598ed
nityananda reddy <nityananda.chappidi@emory.edu>
2018-03-26 13:30:35 -0400

    change to get the httprequest object

--- src/edu/harvard/i2b2/pm/delegate/ServicesHandlerCAS.java
+++ src/edu/harvard/i2b2/pm/delegate/ServicesHandlerCAS.java
@@ -22,6 +22,12 @@ import java.io.IOException;
 import java.util.Hashtable;
 import java.util.Properties;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.axis2.context.MessageContext;
+
+import com.ibm.wsdl.extensions.http.HTTPConstants;
+
 /**
  * ServicesHandlerCAS validates users using
  * JA-SIG Central Authentication Service (CAS).
@@ -69,7 +75,12 @@ public class ServicesHandlerCAS extends ServicesHandler {
 	       || service.startsWith("https:"))){
 	    return super.validateSuppliedPassword(service, ticket, param);
 	}
-
+	
+	MessageContext context = MessageContext.getCurrentMessageContext();
+	HttpServletRequest  request = (HttpServletRequest) context.getProperty("transport.http.servletRequest");
+	System.out.println("+++request+++"+request);
+	log.debug("+++request+++"+request);
+	log.info("+++request+++"+request);
 	String addr = appProperties.getProperty(CAS_URL_PROPERTY_NAME) + "proxyValidate?"
 	    + "service=" + URLEncoder.encode(service, "UTF-8")
 	    + "&ticket=" + URLEncoder.encode(ticket, "UTF-8");
